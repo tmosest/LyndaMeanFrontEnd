410443e54b8565dc1cfee7863cd01d10fa19c049
tmosest <tmosest@gmail.com>
2016-10-08 11:01:29 -0400

    Added Navbar Controller

---
+++ src/app/components/navbar/navbar.controller.js
@@ -0,0 +1,14 @@
+export class NavbarController {
+
+    constructor($auth) {
+        'ngInject';     
+        
+        this.$auth = $auth;
+        this.isAuthenticated = $auth.isAuthenticated;
+    }
+
+    logout() {
+        this.$auth.logout();
+    }
+
+}
